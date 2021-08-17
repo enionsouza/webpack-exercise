8e7dfb099a10efbae5ebf22fda0b5cba15811b0f
Enio N. de Souza <enionsouza@gmail.com>
2021-08-17 15:06:54 -0400

    Add HTML template

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Webpack Exercise',
+      template: './src/index.html',
     }),
   ],
   output: {
